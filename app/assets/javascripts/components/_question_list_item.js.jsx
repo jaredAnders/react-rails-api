576acb0aa21ac34ee334aae1f69051db560cb2e0
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-02-27 17:23:36 -0500

    adds temporary display for answer and distractors

--- app/assets/javascripts/components/_question_list_item.js.jsx
+++ app/assets/javascripts/components/_question_list_item.js.jsx
@@ -1,8 +1,16 @@
 const QuestionListItem = ({question}) => {
+  const distractors = question.distractors.map((distractor) => {
+    return (
+      <span>
+        , {distractor}
+      </span>
+    )
+  })
+
   return (
     <li>
       <p>{question.question_text}</p>
-      {/* <p>{question.answer}</p> */}
+      <p>{question.answer}{distractors}</p>
     </li>
   )
 }
