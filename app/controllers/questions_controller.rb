43914b1e63671f18489e87f1409c95cc506c31d4
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-02-27 11:52:39 -0500

    adds create and update methods to questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,19 @@ class QuestionsController < ApplicationController
   def index
     render json: Question.all
   end
+
+  def create
+    render json: Question.create(question_params)
+  end
+
+  def update
+    question = Question.find(params[:id])
+    render json: question.update_attributes(question_params)
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:question_text, :answer, :distractors)
+  end
 end
