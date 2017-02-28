fc1dd416fc5d210be12c2c2ad728924c6c333ab3
jaredandersgilbert@gmail.com <jaredandersgilbert@gmail.com>
2017-02-28 12:33:32 -0500

    triggers put request to update question via api and update state upon success

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -7,6 +7,8 @@ class Main extends React.Component {
     }
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.onUpdate = this.onUpdate.bind(this);
+    this.updateQuestions = this.updateQuestions.bind(this);
   }
 
   componentDidMount() {
@@ -18,11 +20,29 @@ class Main extends React.Component {
     this.setState({ questions: newState });
   }
 
+  onUpdate(question) {
+    $.ajax({
+      url: `/questions/${question.id}`,
+      type: 'PUT',
+      data: { question },
+      success: () => {
+        this.updateQuestions(question);
+      }
+    })
+  }
+
+  updateQuestions(question) {
+    const questions = this.state.questions.filter((i) => { return i.id != question.id });
+    questions.push(question);
+
+    this.setState({ questions });
+  }
+
   render() {
     return (
       <div>
         <NewQuestion handleSubmit={this.handleSubmit}/>
-        <QuestionList questions={this.state.questions} />
+        <QuestionList questions={this.state.questions} onUpdate={this.onUpdate} />
       </div>
     )
   }
--- app/assets/javascripts/components/_question_list.js.jsx
+++ app/assets/javascripts/components/_question_list.js.jsx
@@ -1,16 +1,28 @@
-const QuestionList = (props) => {
-  const questions = props.questions.map((question) => {
+class QuestionList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
+
+  handleUpdate(question) {
+    this.props.onUpdate(question);
+  }
+
+  render() {
+    const questions = this.props.questions.map((question) => {
+      return (
+        <QuestionListItem
+          key={question.id}
+          question={question}
+          handleUpdate={this.handleUpdate} />
+      )
+    })
+
     return (
-      <QuestionListItem
-        key={question.id}
-        question={question} />
+      <ul>
+        {questions}
+      </ul>
     )
-  })
-
-  return (
-    <ul>
-      {questions}
-    </ul>
-  )
-
+  }
 }
